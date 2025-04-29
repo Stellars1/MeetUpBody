45a925226e4020196e141777bf9e752a3659933a
vgglce <ovgugulecc@gmail.com>
2025-04-29 12:54:41 +0300

    Fixes #39: Implement user profile management

---
+++ users/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'bio', 'location', 'birth_date', 'interests', 'profile_picture']
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+            'bio': forms.Textarea(attrs={'rows': 4}),
+            'interests': forms.CheckboxSelectMultiple()
+        } 
